1c37858a65efb7fb144d8b5eccbb0d721b9f15f3
Ubuntu <ubuntu@ip-172-31-34-20.us-east-2.compute.internal>
2021-07-26 16:37:18 +0000

    modified

---
+++ type.java
@@ -0,0 +1,13 @@
+123456789
+new project folder
+old
+this new profile123456789
+new project folder
+old
+this new profile123456789
+new project folder
+old
+this new profile123456789
+new project folder
+old
+this new profile
